6822d270f31c65682829fa3f315e4756b417c664
Michael Cuttler <00084142@uwa.edu.au>
2020-09-08 15:44:01 +0800

    working on python code...

--- SofarPy/Process_spotter_delayed_mode.py
+++ SofarPy/Process_spotter_delayed_mode.py
@@ -35,15 +35,13 @@ from matplotlib import pyplot as plt
 
 #path for python parser script
 parserpath = os.path.abspath('E:\Active_Projects\LOWE_IMOS_WaveBuoys\Data\SofarSpotter\SofarParser\parser_v1.10.0')
-datapath = os.path.abspath('E:\Active_Projects\LOWE_IMOS_WaveBuoys\Data\SofarSpotter\Data_fr_testing')
+datapath = os.path.abspath('E:\Active_Projects\LOWE_IMOS_WaveBuoys\Data\SofarSpotter\Data_for_testing')
 
 #initiate utput directries 
 
 #meta data about spotter
 SpotterID = 'SPOT0171'
 DeployLoc = 'Torbay'
-StartDate = '20200114'
-EndDate = '20200529'
 
 
 #%% determine number of unique file numbers for chunking data into management number of files
@@ -63,9 +61,8 @@ bulkparams = {'time':[],'hs':[],'tm':[],'tp':[],'dm':[],'dp':[],
               'meanspr':[],'pkspr':[]}
 displacements = {'time':[],'x':[],'y':[],'z':[]}
 locations = {'time':[],'lat':[],'lon':[]}
-specparams = {'a1':[],'b1':[],'a2':[],'b2':[],
-              'Sxx':[],'Syy':[],'Szz':[],'Cxy':[],
-              'Qxz':[],'Qyz':[]}
+spec = {'time':[],'a1':[],'b1':[],'a2':[],'b2':[],
+              'Sxx':[],'Syy':[],'Szz':[],'freq':[]}
 #%%
 for i in range(0, len(file_nums), chunk_size):
     print('Processing files ' + str(i) + ' to ' + str(i+chunk_size) + ' out of ' + str(len(file_nums)))
@@ -78,39 +75,62 @@ for i in range(0, len(file_nums), chunk_size):
     #now process
     runfile('parser_v1.10.0.py')
     
-    dum = np.loadtxt('bulkparameters.csv',delimiter=',',skiprows=1)
-    for j in dum:
-        bulkparams['time'].append(dt(int(j[0]),int(j[1]),int(j[2]),
-                                     int(j[3]),int(j[4]),int(j[5])))
-        bulkparams['hs'].append(j[7])
-        bulkparams['tm'].append(j[8])
-        bulkparams['tp'].append(j[9])
-        bulkparams['dm'].append(j[10])
-        bulkparams['dp'].append(j[11])
-        bulkparams['meanspr'].append(j[12])
-        bulkparams['pkspr'].append(j[13])
+    #check whether parser generated subdirectories or not
+    if os.path.exists('bulkparameters.csv')==False:
+        # ;ljasdf
+    else:
+        dum = np.loadtxt('bulkparameters.csv',delimiter=',',skiprows=1)
+        for j in dum:
+            bulkparams['time'].append(dt(int(j[0]),int(j[1]),int(j[2]),
+                                         int(j[3]),int(j[4]),int(j[5])))
+            bulkparams['hs'].append(j[7])
+            bulkparams['tm'].append(j[8])
+            bulkparams['tp'].append(j[9])
+            bulkparams['dm'].append(j[10])
+            bulkparams['dp'].append(j[11])
+            bulkparams['meanspr'].append(j[12])
+            bulkparams['pkspr'].append(j[13])
     
-    dum = np.loadtxt('displacement.csv',delimiter=',',skiprows=1)
-    for j in dum:
-        displacements['time'].append(dt(int(j[0]),int(j[1]),int(j[2]),
-                                     int(j[3]),int(j[4]),int(j[5])))
-        displacements['x'].append(j[7])
-        displacements['y'].append(j[8])
-        displacements['z'].append(j[9])
+        dum = np.loadtxt('displacement.csv',delimiter=',',skiprows=1)
+        for j in dum:
+            displacements['time'].append(dt(int(j[0]),int(j[1]),int(j[2]),
+                                            int(j[3]),int(j[4]),int(j[5])))
+            displacements['x'].append(j[7])
+            displacements['y'].append(j[8])
+            displacements['z'].append(j[9])
     
-    dum = np.loadtxt('location.csv',delimiter=',',skiprows=1)
-    for j in dum:
-        locations['time'].append(dt(int(j[0]),int(j[1]),int(j[2]),
-                                     int(j[3]),int(j[4]),int(j[5])))
-        locations['lat'].append(j[7])
-        locations['lon'].append(j[8])
+        dum = np.loadtxt('location.csv',delimiter=',',skiprows=1)
+        for j in dum:
+            locations['time'].append(dt(int(j[0]),int(j[1]),int(j[2]),
+                                        int(j[3]),int(j[4]),int(j[5])))
+            locations['lat'].append(j[7])
+            locations['lon'].append(j[8])    
     
+        filenames = ['a1','a2','b1','b2','Sxx','Syy','Szz']
+        for j,file in enumerate(filenames):
+            if j==0:            
+                f = csv.DictReader(file+'.csv')
+                spec['freq'] = [float(i) for i in f.fieldnames[8:]]
+                dum = np.loadtxt(file+'.csv',delimiter=',',skiprows=1)
+                
+                for j in dum: 
+                    spec['time'].append(dt(int(j[0]),int(j[1]),int(j[2]),
+                                            int(j[3]),int(j[4]),int(j[5])))
+                spec[file] = np.array(dum[:,8:])
+            
+            else:
+                dum = np.loadtxt(file+'.csv',delimiter=',',skiprows=1)
+                spec[file] = np.array(dum[:,8:])
+        
+      
     #delete files from tmp directory
     for i in os.listdir(tmp_path):
         if i[-3:]=='csv':
             os.remove(i)
         elif i[-3:]=='CSV':
             os.remove(i)
+    
+    %clear        
             
 #%% now output dictionaries to netCDF       
 
