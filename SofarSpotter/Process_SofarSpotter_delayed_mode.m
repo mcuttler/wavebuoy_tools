e4b60f0fa52b17ef2b4ef14f34aecbfd57d8226a
mcuttler <michael.cuttler@uwa.edu.au>
2020-10-06 17:01:42 +0800

    update for first 'final' version of workflow for bulk parameters

--- SofarSpotter/Process_SofarSpotter_delayed_mode.m
+++ SofarSpotter/Process_SofarSpotter_delayed_mode.m
@@ -34,7 +34,7 @@
 %                                                                          first QARTOD QA/QC tests - Time series bulk wave parameters
 %                                                                          max/min/acceptable range (test 19, required)
 %
-
+% 
 %% set initial paths for Spotter data to process and parser script
 clear; clc
 %path to Spotter data to process - contains raw dump of SD card (_SYS,_FLT,
@@ -44,8 +44,8 @@ datapath = 'E:\Active_Projects\LOWE_IMOS_WaveBuoys\Data\SofarSpotter\CodeTesting
 parserpath = 'E:\Active_Projects\LOWE_IMOS_WaveBuoys\Data\SofarSpotter\SofarParser\parser_v1.10.0';
 
 %path to save netCDF files for transfer to AODN
-outpathNC = 'E:\Active_Projects\LOWE_IMOS_WaveBuoys\Data\SofarSpotter\ProcessedData_DelayedMode\nc';
-outpathMAT = 'E:\Active_Projects\LOWE_IMOS_WaveBuoys\Data\SofarSpotter\ProcessedData_DelayedMode\mat';
+outpathNC = 'E:\Active_Projects\LOWE_IMOS_WaveBuoys\Data\SofarSpotter\CodeTesting\Output_testing';
+outpathMAT = 'E:\Active_Projects\LOWE_IMOS_WaveBuoys\Data\SofarSpotter\CodeTesting\Output_testing';
 
 if ~exist(outpathNC)
     mkdir(outpathNC)
@@ -58,6 +58,7 @@ end
 %spotter serial number and deployment info 
 spot_info.SpotterID = 'SPOT0171'; 
 spot_info.DeployLoc = 'Testing';
+spot_info.DeployDepth = 30; 
 
 %location of wavebuoy_tools repo
 homepath = 'D:\CUTTLER_GitHub\wavebuoy_tools\SofarSpotter'; 
@@ -319,8 +320,6 @@ for i = 1:size(tdata,1)
     tend = datenum(tdata(i,1), tdata(i,2)+1, 1); 
     
     disp(['Saving ' datestr(tstart,'mmm yyyy')]); 
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     %output for BULK PARAMETERS     
     idx_bulk = []; 
@@ -330,146 +329,145 @@ for i = 1:size(tdata,1)
     globfile = 'D:\CUTTLER_GitHub\wavebuoy_tools\SofarSpotter\glob_att_Spotter_timeSeries.txt';     
     varsfile = 'D:\CUTTLER_GitHub\wavebuoy_tools\SofarSpotter\spotter_wave_parameters_mapping.csv';
     
-    spotter_to_IMOSnc(bulkparams, idx_bulk, filenameNC, globfile, varsfile); 
+    spotter_bulk_to_IMOSnc(bulkparams, idx_bulk, filenameNC, globfile, varsfile); 
     
-      
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %output netCDF for DISPLACEMENTS
-    idx_disp = []; 
-    idx_disp = find(displacements.time>=tstart&displacements.time<tend);                 
-    filenameNC = [outpathNC '\' SpotterID '_' DeployLoc '_' datestr(tstart,'yyyymm') '_disp.nc']; 
-    
-    [m,c] = size(displacements.time(idx_disp)); 
-    nccreate(filenameNC,'disp_time','Dimensions',{'time',m});
-    nccreate(filenameNC,'x','Dimensions',{'x',m}); 
-    nccreate(filenameNC,'y','Dimensions',{'y',m}); 
-    nccreate(filenameNC,'z','Dimensions',{'z',m});     
-    nccreate(filenameNC,'QualityFlag','Dimensions',{'QualityFlag',m}); 
-    
-    ncwrite(filenameNC,'disp_time',displacements.time(idx_disp)); 
-    ncwriteatt(filenameNC,'disp_time','long_name','UTC');  
-    ncwriteatt(filenameNC,'disp_time','units','days since Jan-1-0000, includes milliseconds');    
-    
-    ncwrite(filenameNC,'x',displacements.x(idx_disp)); 
-    ncwriteatt(filenameNC,'x','long_name','x displacement');  
-    ncwriteatt(filenameNC,'x','units','m');
-    
-    ncwrite(filenameNC,'y',displacements.y(idx_disp));           
-    ncwriteatt(filenameNC,'y','long_name','y displacement');  
-    ncwriteatt(filenameNC,'y','units','m');   
-    
-    ncwrite(filenameNC,'z',displacements.z(idx_disp)); 
-    ncwriteatt(filenameNC,'z','long_name','z displacement');  
-    ncwriteatt(filenameNC,'z','units','m');
-    
-    ncwrite(filenameNC,'QualityFlag',qfdisp(idx_disp)); 
-    ncwriteatt(filenameNC,'QualityFlag','long_name','quality flag: COMPLETE WHEN QUALITY FLAGS ARE FINISHED');  
-    ncwriteatt(filenameNC,'QualityFlag','units','-');
-    
-        
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %output netCDF for LOCATIONS
-    idx_locs = []; 
-    idx_locs = find(locations.time>=tstart&locations.time<tend);                 
-    filenameNC = [outpathNC '\' SpotterID '_' DeployLoc '_' datestr(tstart,'yyyymm') '_gps.nc']; 
-    
-    [m,c] = size(locations.time(idx_locs)); 
-    nccreate(filenameNC,'time','Dimensions',{'time',m});
-    nccreate(filenameNC,'lat','Dimensions',{'lat',m}); 
-   nccreate(filenameNC,'lon','Dimensions',{'lon',m});   
-    nccreate(filenameNC,'QualityFlag','Dimensions',{'QualityFlag',m}); 
-    
-    ncwrite(filenameNC,'time',locations.time(idx_locs)); 
-    ncwriteatt(filenameNC,'time','long_name','UTC');  
-    ncwriteatt(filenameNC,'time','units','days since Jan-1-0000, includes milliseconds');    
-    
-    ncwrite(filenameNC,'lat',locations.lat(idx_locs)); 
-    ncwriteatt(filenameNC,'lat','long_name','latitude');  
-    ncwriteatt(filenameNC,'lat','units','deg');
-    
-    ncwrite(filenameNC,'lon', locations.lon(idx_locs)); 
-    ncwriteatt(filenameNC,'lon','long_name','longitude');  
-    ncwriteatt(filenameNC,'lon','units','m');
-    
-    ncwrite(filenameNC,'QualityFlag',qflocs(idx_locs)); 
-    ncwriteatt(filenameNC,'QualityFlag','long_name','quality flag: COMPLETE WHEN QUALITY FLAGS ARE FINISHED');  
-    ncwriteatt(filenameNC,'QualityFlag','units','-');
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %output netCDF for SPECTRAL DATA
-    idx_spec = []; 
-    idx_spec = find(spec.time>=tstart&spec.time<tend);                 
-    filenameNC = [outpathNC '\' SpotterID '_' DeployLoc '_' datestr(tstart,'yyyymm') '_spec.nc']; 
-    
-    [m,c] = size(spec.a1(idx_spec,:)); 
-
-    nccreate(filenameNC,'time','Dimensions',{'time',m});
-    nccreate(filenameNC,'a1','Dimensions',{'time',m,'freq',c}); 
-    nccreate(filenameNC,'a2','Dimensions',{'time',m,'freq',c}); 
-    nccreate(filenameNC,'b1','Dimensions',{'time',m,'freq',c}); 
-    nccreate(filenameNC,'b2','Dimensions',{'time',m,'freq',c}); 
-    nccreate(filenameNC,'Sxx','Dimensions',{'time',m,'freq',c}); 
-    nccreate(filenameNC,'Syy','Dimensions',{'time',m,'freq',c}); 
-    nccreate(filenameNC,'Szz','Dimensions',{'time',m,'freq',c}); 
-    nccreate(filenameNC,'freq','Dimensions',{'time',m,'freq',c}); 
-    nccreate(filenameNC,'QualityFlag','Dimensions',{'QualityFlag',m}); 
-    
-    ncwrite(filenameNC,'time', spec.time(idx_spec)); 
-    ncwriteatt(filenameNC,'time','long_name','UTC');  
-    ncwriteatt(filenameNC,'time','units','days since Jan-1-0000, includes milliseconds');    
-    
-    ncwrite(filenameNC,'a1', spec.a1(idx_spec,:)); 
-    ncwriteatt(filenameNC,'a1','long_name','spectral coefficient a1');  
-    ncwriteatt(filenameNC,'a1','units','-');    
-        
-    ncwrite(filenameNC,'b1', spec.b1(idx_spec,:)); 
-    ncwriteatt(filenameNC,'b1','long_name','spectral coefficient a1');  
-    ncwriteatt(filenameNC,'b1','units','-');
-        
-    ncwrite(filenameNC,'a2', spec.a2(idx_spec,:)); 
-    ncwriteatt(filenameNC,'a2','long_name','spectral coefficient a1');  
-    ncwriteatt(filenameNC,'a2','units','-');    
-        
-    ncwrite(filenameNC,'b2', spec.b2(idx_spec,:)); 
-    ncwriteatt(filenameNC,'b2','long_name','spectral coefficient a1');  
-    ncwriteatt(filenameNC,'b2','units','-');    
-        
-    ncwrite(filenameNC,'Sxx', spec.Sxx(idx_spec,:)); 
-    ncwriteatt(filenameNC,'Sxx','long_name','spectral coefficient a1');  
-    ncwriteatt(filenameNC,'Sxx','units','-');    
-        
-    ncwrite(filenameNC,'Syy', spec.Syy(idx_spec,:)); 
-    ncwriteatt(filenameNC,'Syy','long_name','spectral coefficient a1');  
-    ncwriteatt(filenameNC,'Syy','units','-');
-        
-    ncwrite(filenameNC,'Szz', spec.Szz(idx_spec,:)); 
-    ncwriteatt(filenameNC,'Szz','long_name','spectral coefficient a1');  
-    ncwriteatt(filenameNC,'Szz','units','-');        
-    
-    %frequency is longer than spectral coefficients because Sofar pads with
-    %nans
-    ncwrite(filenameNC,'freq', spec.freq(1:c)); 
-    ncwriteatt(filenameNC,'freq','long_name','frequency');  
-    ncwriteatt(filenameNC,'freq','units','Hz'); 
-
-    ncwrite(filenameNC,'QualityFlag',qf(idx_spec,3));  
-    ncwriteatt(filenameNC,'QualityFlag','long_name','quality flag: 0 = good data, 1 = problem with wave height or period, 2 = problem with wave height and period');  
-    ncwriteatt(filenameNC,'QualityFlag','units','-');
-    
-    %only save .mat file at the end - contains all data, not split by
-    %monthly 
-    if i == size(tdata,1)
-        filenameMAT = [outpathMAT '\' SpotterID '_' DeployLoc '_' datestr(tstart,'yyyymm') '_' datestr(tend,'yyyymm') '.mat']; 
-        save(filenameMAT,'bulkparams','spec','locations','displacements','-v7.3'); 
-    end
-        
 end
-    
-%clear command window and displayed finished processing
-clc; 
+
+%% output netCDF for DISPLACEMENTS - write into code to make IMOS-compliant
+%     idx_disp = []; 
+%     idx_disp = find(displacements.time>=tstart&displacements.time<tend);                 
+%     filenameNC = [outpathNC '\' SpotterID '_' DeployLoc '_' datestr(tstart,'yyyymm') '_disp.nc']; 
+%     
+%     [m,c] = size(displacements.time(idx_disp)); 
+%     nccreate(filenameNC,'disp_time','Dimensions',{'time',m});
+%     nccreate(filenameNC,'x','Dimensions',{'x',m}); 
+%     nccreate(filenameNC,'y','Dimensions',{'y',m}); 
+%     nccreate(filenameNC,'z','Dimensions',{'z',m});     
+%     nccreate(filenameNC,'QualityFlag','Dimensions',{'QualityFlag',m}); 
+%     
+%     ncwrite(filenameNC,'disp_time',displacements.time(idx_disp)); 
+%     ncwriteatt(filenameNC,'disp_time','long_name','UTC');  
+%     ncwriteatt(filenameNC,'disp_time','units','days since Jan-1-0000, includes milliseconds');    
+%     
+%     ncwrite(filenameNC,'x',displacements.x(idx_disp)); 
+%     ncwriteatt(filenameNC,'x','long_name','x displacement');  
+%     ncwriteatt(filenameNC,'x','units','m');
+%     
+%     ncwrite(filenameNC,'y',displacements.y(idx_disp));           
+%     ncwriteatt(filenameNC,'y','long_name','y displacement');  
+%     ncwriteatt(filenameNC,'y','units','m');   
+%     
+%     ncwrite(filenameNC,'z',displacements.z(idx_disp)); 
+%     ncwriteatt(filenameNC,'z','long_name','z displacement');  
+%     ncwriteatt(filenameNC,'z','units','m');
+%     
+%     ncwrite(filenameNC,'QualityFlag',qfdisp(idx_disp)); 
+%     ncwriteatt(filenameNC,'QualityFlag','long_name','quality flag: COMPLETE WHEN QUALITY FLAGS ARE FINISHED');  
+%     ncwriteatt(filenameNC,'QualityFlag','units','-');
+%     
+%         
+
+%% output netCDF for LOCATIONS
+%     idx_locs = []; 
+%     idx_locs = find(locations.time>=tstart&locations.time<tend);                 
+%     filenameNC = [outpathNC '\' SpotterID '_' DeployLoc '_' datestr(tstart,'yyyymm') '_gps.nc']; 
+%     
+%     [m,c] = size(locations.time(idx_locs)); 
+%     nccreate(filenameNC,'time','Dimensions',{'time',m});
+%     nccreate(filenameNC,'lat','Dimensions',{'lat',m}); 
+%    nccreate(filenameNC,'lon','Dimensions',{'lon',m});   
+%     nccreate(filenameNC,'QualityFlag','Dimensions',{'QualityFlag',m}); 
+%     
+%     ncwrite(filenameNC,'time',locations.time(idx_locs)); 
+%     ncwriteatt(filenameNC,'time','long_name','UTC');  
+%     ncwriteatt(filenameNC,'time','units','days since Jan-1-0000, includes milliseconds');    
+%     
+%     ncwrite(filenameNC,'lat',locations.lat(idx_locs)); 
+%     ncwriteatt(filenameNC,'lat','long_name','latitude');  
+%     ncwriteatt(filenameNC,'lat','units','deg');
+%     
+%     ncwrite(filenameNC,'lon', locations.lon(idx_locs)); 
+%     ncwriteatt(filenameNC,'lon','long_name','longitude');  
+%     ncwriteatt(filenameNC,'lon','units','m');
+%     
+%     ncwrite(filenameNC,'QualityFlag',qflocs(idx_locs)); 
+%     ncwriteatt(filenameNC,'QualityFlag','long_name','quality flag: COMPLETE WHEN QUALITY FLAGS ARE FINISHED');  
+%     ncwriteatt(filenameNC,'QualityFlag','units','-');
+%     
+%     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%     %output netCDF for SPECTRAL DATA
+%     idx_spec = []; 
+%     idx_spec = find(spec.time>=tstart&spec.time<tend);                 
+%     filenameNC = [outpathNC '\' SpotterID '_' DeployLoc '_' datestr(tstart,'yyyymm') '_spec.nc']; 
+%     
+%     [m,c] = size(spec.a1(idx_spec,:)); 
+% 
+%     nccreate(filenameNC,'time','Dimensions',{'time',m});
+%     nccreate(filenameNC,'a1','Dimensions',{'time',m,'freq',c}); 
+%     nccreate(filenameNC,'a2','Dimensions',{'time',m,'freq',c}); 
+%     nccreate(filenameNC,'b1','Dimensions',{'time',m,'freq',c}); 
+%     nccreate(filenameNC,'b2','Dimensions',{'time',m,'freq',c}); 
+%     nccreate(filenameNC,'Sxx','Dimensions',{'time',m,'freq',c}); 
+%     nccreate(filenameNC,'Syy','Dimensions',{'time',m,'freq',c}); 
+%     nccreate(filenameNC,'Szz','Dimensions',{'time',m,'freq',c}); 
+%     nccreate(filenameNC,'freq','Dimensions',{'time',m,'freq',c}); 
+%     nccreate(filenameNC,'QualityFlag','Dimensions',{'QualityFlag',m}); 
+%     
+%     ncwrite(filenameNC,'time', spec.time(idx_spec)); 
+%     ncwriteatt(filenameNC,'time','long_name','UTC');  
+%     ncwriteatt(filenameNC,'time','units','days since Jan-1-0000, includes milliseconds');    
+%     
+%     ncwrite(filenameNC,'a1', spec.a1(idx_spec,:)); 
+%     ncwriteatt(filenameNC,'a1','long_name','spectral coefficient a1');  
+%     ncwriteatt(filenameNC,'a1','units','-');    
+%         
+%     ncwrite(filenameNC,'b1', spec.b1(idx_spec,:)); 
+%     ncwriteatt(filenameNC,'b1','long_name','spectral coefficient a1');  
+%     ncwriteatt(filenameNC,'b1','units','-');
+%         
+%     ncwrite(filenameNC,'a2', spec.a2(idx_spec,:)); 
+%     ncwriteatt(filenameNC,'a2','long_name','spectral coefficient a1');  
+%     ncwriteatt(filenameNC,'a2','units','-');    
+%         
+%     ncwrite(filenameNC,'b2', spec.b2(idx_spec,:)); 
+%     ncwriteatt(filenameNC,'b2','long_name','spectral coefficient a1');  
+%     ncwriteatt(filenameNC,'b2','units','-');    
+%         
+%     ncwrite(filenameNC,'Sxx', spec.Sxx(idx_spec,:)); 
+%     ncwriteatt(filenameNC,'Sxx','long_name','spectral coefficient a1');  
+%     ncwriteatt(filenameNC,'Sxx','units','-');    
+%         
+%     ncwrite(filenameNC,'Syy', spec.Syy(idx_spec,:)); 
+%     ncwriteatt(filenameNC,'Syy','long_name','spectral coefficient a1');  
+%     ncwriteatt(filenameNC,'Syy','units','-');
+%         
+%     ncwrite(filenameNC,'Szz', spec.Szz(idx_spec,:)); 
+%     ncwriteatt(filenameNC,'Szz','long_name','spectral coefficient a1');  
+%     ncwriteatt(filenameNC,'Szz','units','-');        
+%     
+%     %frequency is longer than spectral coefficients because Sofar pads with
+%     %nans
+%     ncwrite(filenameNC,'freq', spec.freq(1:c)); 
+%     ncwriteatt(filenameNC,'freq','long_name','frequency');  
+%     ncwriteatt(filenameNC,'freq','units','Hz'); 
+% 
+%     ncwrite(filenameNC,'QualityFlag',qf(idx_spec,3));  
+%     ncwriteatt(filenameNC,'QualityFlag','long_name','quality flag: 0 = good data, 1 = problem with wave height or period, 2 = problem with wave height and period');  
+%     ncwriteatt(filenameNC,'QualityFlag','units','-');
+%     
+%     %only save .mat file at the end - contains all data, not split by
+%     %monthly 
+%     if i == size(tdata,1)
+%         filenameMAT = [outpathMAT '\' SpotterID '_' DeployLoc '_' datestr(tstart,'yyyymm') '_' datestr(tend,'yyyymm') '.mat']; 
+%         save(filenameMAT,'bulkparams','spec','locations','displacements','-v7.3'); 
+%     end
+%         
+% % end
+%     
+% %clear command window and displayed finished processing
+% clc; 
+%%
 disp(['Finished processing ' SpotterID ' delayed mode']); 
 
 %% plot quick figure
