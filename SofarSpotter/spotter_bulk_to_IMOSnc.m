1baffe63f0d1820f1f420fcbd0ab7604262eb671
mcuttler <michael.cuttler@uwa.edu.au>
2020-10-08 11:07:55 +0800

    updating structure for running

--- SofarSpotter/spotter_bulk_to_IMOSnc.m
+++ SofarSpotter/spotter_bulk_to_IMOSnc.m
@@ -15,96 +15,111 @@
 
 %%
 
-function [] = spotter_bulk_to_IMOSnc(bulkparams, idx_bulk, filenameNC, globfile, varsfile); 
+function [] = spotter_bulk_to_IMOSnc(bulkparams, outpathNC, spot_info, globfile, varsfile); 
 
+%determine min and max months
+t = datevec(bulkparams.time); 
+tdata = unique(t(:,1:2),'rows'); 
 
-%create output netCDF4 file     
-ncid = netcdf.create(filenameNC,'NETCDF4'); 
-netcdf.close(ncid); 
-
-%% global attributes
-
-fid = fopen(globfile); 
-globatts = textscan(fid, '%s%s','delimiter','='); 
-fclose(fid);
-
-ncid = netcdf.open(filenameNC,'WRITE'); 
-varid = netcdf.getConstant('GLOBAL');
-
-for ii = 1:size(globatts{1,1},1);
+for i = 1:size(tdata,1)
+ 
+    tstart = datenum(tdata(i,1), tdata(i,2),1); 
+    tend = datenum(tdata(i,1), tdata(i,2)+1, 1); 
+    
+    disp(['Saving ' datestr(tstart,'mmm yyyy')]); 
+    
+    %output for BULK PARAMETERS     
+    idx_bulk = []; 
+    idx_bulk = find(bulkparams.time>=tstart&bulkparams.time<tend);                 
+    filenameNC = [outpathNC '\' spot_info.SpotterID '_' spot_info.DeployLoc '_' datestr(tstart,'yyyymm') '_bulk.nc'];     
+    
+    
+    
+    
+    %create output netCDF4 file     
+    ncid = netcdf.create(filenameNC,'NETCDF4'); 
+    netcdf.close(ncid); 
+    
+    %% global attributes
+    
+    fid = fopen(globfile); 
+    globatts = textscan(fid, '%s%s','delimiter','='); 
+    fclose(fid);
+    
+    ncid = netcdf.open(filenameNC,'WRITE'); 
+    varid = netcdf.getConstant('GLOBAL');
+    
+    for ii = 1:size(globatts{1,1},1);
         
-    attname = globatts{1,1}{ii};
-    %get rid of trailing spaces
-    idx = find(attname~= ' '); 
-    attname = attname(idx); 
-    attvalue = globatts{1,2}{ii}; 
-    
-    netcdf.putAtt(ncid,varid, attname, attvalue);  
-    
-    if ii == size(globatts{1,1},1);
-        netcdf.putAtt(ncid,varid, 'deployment_depth', '-30 m'); 
-        netcdf.putAtt(ncid,varid, 'deployment_latitude', num2str(nanmean(bulkparams.lat))); 
-        netcdf.putAtt(ncid,varid, 'deployment_longitude', num2str(nanmean(bulkparams.lon))); 
+        attname = globatts{1,1}{ii};
+        %get rid of trailing spaces
+        idx = find(attname~= ' '); 
+        attname = attname(idx); 
+        attvalue = globatts{1,2}{ii}; 
+        
+        netcdf.putAtt(ncid,varid, attname, attvalue);  
+        
+        if ii == size(globatts{1,1},1);
+            netcdf.putAtt(ncid,varid, 'deployment_depth', '-30 m'); 
+            netcdf.putAtt(ncid,varid, 'deployment_latitude', num2str(nanmean(bulkparams.lat))); 
+            netcdf.putAtt(ncid,varid, 'deployment_longitude', num2str(nanmean(bulkparams.lon))); 
+        end
+        
     end
     
-end
-
-
-%%
-% define dimensions 
-
-dimname = 'TIME';
-dimlength = size(bulkparams.time(idx_bulk),1);
-
-dimid_TIME = netcdf.defDim(ncid, dimname, dimlength); 
-
-% write variables 
-
-%parameter mapping file organised as: 
-% [original name, varname, standard name, long name, units, comments, ancillary invo, valid min, valid max, reference, positive]
-
-fid = fopen(varsfile); 
-varinfo = textscan(fid, '%s%s%s%s%s%s%s%f%f%s%s','delimiter',',','headerlines',1); 
-fclose(fid);      
-
-attnames = {'standard_name', 'long_name', 'units', 'comments', 'ancillary_variables', 'valid_min', 'valid_max', 'reference', 'positive'}; 
-attinfo = varinfo(3:end); 
-
-[m,~] = size(varinfo{1,1}); 
-for ii = 1:m    
-    %create and define variable and attributes      
-    netcdf.defVar(ncid, varinfo{1,2}{ii,1}, 'double', dimid_TIME); 
     
-    varid = netcdf.inqVarID(ncid,varinfo{1,2}{ii});   
+    %%
+    % define dimensions 
     
-    %add attributes
-    for j = 1:length(attinfo); 
-        if j==6|j==7
-            if ~isnan(attinfo{1,j}(ii))
-                netcdf.putAtt(ncid, varid, attnames{j},attinfo{1,j}(ii));
-            end
-        else
-            if ~isempty(attinfo{1,j}{ii})
-                netcdf.putAtt(ncid, varid, attnames{j},attinfo{1,j}{ii});
+    dimname = 'TIME';
+    dimlength = size(bulkparams.time(idx_bulk),1);
+    
+    dimid_TIME = netcdf.defDim(ncid, dimname, dimlength); 
+    
+    % write variables 
+    
+    %parameter mapping file organised as: 
+    % [original name, varname, standard name, long name, units, comments, ancillary invo, valid min, valid max, reference, positive]
+    
+    fid = fopen(varsfile); 
+    varinfo = textscan(fid, '%s%s%s%s%s%s%s%f%f%s%s','delimiter',',','headerlines',1); 
+    fclose(fid);      
+    
+    attnames = {'standard_name', 'long_name', 'units', 'comments', 'ancillary_variables', 'valid_min', 'valid_max', 'reference', 'positive'}; 
+    attinfo = varinfo(3:end); 
+    
+    [m,~] = size(varinfo{1,1}); 
+    for ii = 1:m    
+        %create and define variable and attributes      
+        netcdf.defVar(ncid, varinfo{1,2}{ii,1}, 'double', dimid_TIME); 
+        
+        varid = netcdf.inqVarID(ncid,varinfo{1,2}{ii});   
+        
+        %add attributes
+        for j = 1:length(attinfo); 
+            if j==6|j==7
+                if ~isnan(attinfo{1,j}(ii))
+                    netcdf.putAtt(ncid, varid, attnames{j},attinfo{1,j}(ii));
+                end
+            else
+                if ~isempty(attinfo{1,j}{ii})
+                    netcdf.putAtt(ncid, varid, attnames{j},attinfo{1,j}{ii});
+                end
             end
         end
+        
+        %put data to variable
+        if strcmp(varinfo{1,1}{ii,1},'temp')
+            %modify this for spotter v2
+            netcdf.putVar(ncid, varid, ones(size(bulkparams.time(idx_bulk))).*nan); 
+        else
+            netcdf.putVar(ncid, varid, bulkparams.(varinfo{1,1}{ii,1})(idx_bulk));
+        end
+        
     end
-
-    %put data to variable
-    if strcmp(varinfo{1,1}{ii,1},'temp')
-        %modify this for spotter v2
-        netcdf.putVar(ncid, varid, ones(size(bulkparams.time(idx_bulk))).*nan); 
-    else
-        netcdf.putVar(ncid, varid, bulkparams.(varinfo{1,1}{ii,1})(idx_bulk));
-    end
-
-end
     
-netcdf.close(ncid); 
-
-% check that it all worked 
-
-ncdisp(filenameNC)
+    netcdf.close(ncid);    
+end
 end
 
 
