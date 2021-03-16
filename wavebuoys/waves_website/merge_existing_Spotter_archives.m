70e7117a66c0f4d706f0dc1580cb9cf41edf9d05
mcuttler <michael.cuttler@uwa.edu.au>
2021-03-16 19:21:44 +0800

    update merge_spotter_archive

--- wavebuoys/waves_website/merge_existing_Spotter_archives.m
+++ wavebuoys/waves_website/merge_existing_Spotter_archives.m
@@ -14,7 +14,7 @@ if strcmp(buoy_info.type,'sofar')
             'dmspr',[],'lat',[],'lon',[],'wind_speed',[],'wind_dir',[],'wind_seasurfaceId',[]); 
     end
     
-    
+    %loop through existing years/months 
     fields = fieldnames(original_data);
     if exist(original_archive_path)
         original_yrs = dir(original_archive_path); original_yrs = original_yrs(3:end); 
@@ -39,15 +39,35 @@ if strcmp(buoy_info.type,'sofar')
                 end
             end
         end
-    end
-    
-    
-    
-    
-%% Datawell    
+    end              
+% Datawell    
 elseif strcmp(buoy_info.type,'datawell')
     original_archive_path = 'E:\DatawellBuoys\TorbayInshore\MAT'; 
 end
+%% load current formatted data
+mat_years = dir([buoy_info.archive_path '\' buoy_info.name '\mat_archive']); 
+mat_years = mat_years(3:end); 
+for i = 1:size(mat_years,1); 
+    mat_months = dir([buoy_info.archive_path '\' buoy_info.name '\mat_archive\' mat_years(i).name]); 
+    mat_months = mat_months(3:end); 
+    for ii = 1:size(mat_months,1)
+        load([buoy_info.archive_path '\' buoy_info.name '\mat_archive\' mat_years(i).name '\' mat_months(ii).name]); 
+        if i == 1
+            current_data = SpotData;
+        else
+            fields = fieldnames(SpotData);
+            for j = 1:length(fields)
+                if isfields(current_data, fields{j])
+                    current_data.(fields{j]) = [current_data.(fields{j}); SpotData.(fields{j})]; 
+                else
+                    current_data.(fields{j}) = SpotData.(fields{j]); 
+                end
+            end
+        end
+    end
+end
+
+%%
 
 
 
