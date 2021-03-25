abb341cb2340441b81aac95364c30e7eae89cc87
mcuttler <michael.cuttler@uwa.edu.au>
2021-03-25 15:30:23 +0800

    modify remove duplicates

--- wavebuoys/waves_website/remove_duplicates.m
+++ wavebuoys/waves_website/remove_duplicates.m
@@ -15,10 +15,10 @@ for i = 1:length(t_wave)
     dataout.time(i,1) = t_wave(i); 
     idx = find(data.time==t_wave(i));     
     for j = 1:length(fields)
-        if length(idx)>1
-            if strcmp(fields{j},'temp_time')|strcmp(fields{j},'surf_temp')|strcmp(fields{j},'bott_temp')|strcmp(fields{j},'qf_sst')|strcmp(fields{j},'qf_bott_temp')
-                dum = 1;
-            else
+        if strcmp(fields{j},'temp_time')|strcmp(fields{j},'surf_temp')|strcmp(fields{j},'bott_temp')|strcmp(fields{j},'qf_sst')|strcmp(fields{j},'qf_bott_temp')
+            dum = 1;
+        else
+            if length(idx)>1            
                 t1 = data.(fields{j})(idx(1));
                 t2 = data.(fields{j})(idx(2));                 
                 if isnan(t1)&isnan(t2)
@@ -27,12 +27,12 @@ for i = 1:length(t_wave)
                     dataout.(fields{j}) = [dataout.(fields{j}); data.(fields{j})(idx(2))]; 
                 elseif ~isnan(t1)&isnan(t2)
                     dataout.(fields{j}) = [dataout.(fields{j}); data.(fields{j})(idx(1))]; 
-                else
+                else                
                     dataout.(fields{j})=[dataout.(fields{j}); data.(fields{j})(idx(1))];
                 end
+            else
+                dataout.(fields{j}) = [dataout.(fields{j}); data.(fields{j})(idx)];
             end
-        else
-            dataout.(fields{j}) = [dataout.(fields{j}); data.(fields{j})(idx)];
         end
     end
     clear idx t1 t2 
