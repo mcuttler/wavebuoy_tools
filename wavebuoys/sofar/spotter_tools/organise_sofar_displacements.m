47bb2394a417be343daaaffba6fdd0e8e0ac426c
mcuttler <michael.cuttler@uwa.edu.au>
2021-11-28 19:57:19 +0800

    update organise displacements code

--- wavebuoys/sofar/spotter_tools/organise_sofar_displacements.m
+++ wavebuoys/sofar/spotter_tools/organise_sofar_displacements.m
@@ -8,18 +8,24 @@
 %% data paths and buoy to process 
 clear; clc; 
 % lcoation of all 'raw' delayed mode data for Spotters
-datapath = 'X:\LOWE_IMOS_Deakin_Collab_JUN2020\Data\SofarSpotter\RAW_delayed_mode'; 
+datapath = 'I:\Active_Projects\LOWE_IMOS_WaveBuoys\Data\SofarSpotter\RAW_delayed_mode'; 
 
 %sofar parser 
-parserpath = 'X:\LOWE_IMOS_Deakin_Collab_JUN2020\Data\SofarSpotter\SofarParser\parser_v1.12.0'; 
+parserpath = 'I:\Active_Projects\LOWE_IMOS_WaveBuoys\Data\SofarSpotter\SofarParser\parser_v1.12.0'; 
 parser = 'parser_v1.12.0.py'; 
 
 % specific folders for buoys to process, uncomment for each buoy - could
 % process all together, but easier for now to do one at a time
 
 %Torbay East
-spotname = 'TorbayEast'; 
-spotdata = {'SPOT0171_TorbayEast_20200319_to_20200529';'SPOT0171_TorbayEast_20210212_to_20210712';'SPOT0559_TorbayEast_20210710_to_20210907'}; 
+spotname = {'TorbayEast','TorbayWest','CapeBW','Tantabiddi'};
+spotdata= {'SPOT0171_TorbayEast_20200319_to_20200529','SPOT0171_TorbayEast_20210212_to_20210712','SPOT0559_TorbayEast_20210710_to_20210907'}; 
+spotdata{2,1} =  'SPOT0172_TorbayWest_20190131_to_20200316'; 
+spotdata{2,2} = 'SPOT0757_TorbayWest_20210212_to_20210712'; 
+spotdata{2,3} = 'SPOT0757_TorbayWest_20210712_to_20210827'; 
+spotdata{3,1} = 'SPOT0297_CapeBridgewater_20191124_to_20200722';
+spotdata{4,1} = 'SPOT0558_Tantabiddi_20201101_20210401';
+
 %Torbay West 
 % spotname = 'TorbayWest'; 
 % spotdata = {'SPOT0172_TorbayWest_20190131_to_20200316';'SPOT0757_TorbayWest_20210212_to_20210712';'SPOT0757_TorbayWest_20210712_to_20210827'}; 
@@ -31,44 +37,74 @@ spotdata = {'SPOT0171_TorbayEast_20200319_to_20200529';'SPOT0171_TorbayEast_2021
 % spotdata = {'SPOT0558_Tantabiddi_20201101_20210401'}; 
 
 %% get displacement data 
+clc;
 
-for k = 1:length(spotdata)
-    clc; 
-    sofarpath = fullfile(datapath,spotdata{k},'Raw'); 
-    
-    [bulkparams, displacements, locations, spec, ~] = process_SofarSpotter_delayed_mode(sofarpath, parserpath, parser, 10);
-    
-    
-    %save to dout
-    if k == 1
-        dout = ddisp; 
-    else
-        fields = fieldnames(dout); 
-        for jj = 1:length(fields)
-            dout.(fields{jj}) = [dout.(fields{jj}); ddisp.(fields{jj})]; 
+for s = 1:length(spotname)
+    disp(['Getting data for ' spotname{s}]); 
+    for k = 1:size(spotdata,2)
+        clc; 
+        sofarpath = fullfile(datapath,spotdata{s,k},'Raw'); 
+        
+        %     [bulkparams, displacements, locations, spec, ~] = process_SofarSpotter_delayed_mode(sofarpath, parserpath, parser, 10);
+        [~, displacements, ~, ~, ~] = process_SofarSpotter_delayed_mode(sofarpath, parserpath, parser, 10);
+        
+        
+        %save to dout
+        if k == 1
+            dout = displacements; 
+        else
+            fields = fieldnames(dout);
+            for jj = 1:length(fields)
+                dout.(fields{jj}) = [dout.(fields{jj}); displacements.(fields{jj})]; 
+            end
         end
+        
+        %rename to buoy name if last buoy folder
+        if k == size(spotdata,2)
+            eval([spotname{s} '=dout;']); 
+        end
+        clear displacements
     end
     
-    %rename to buoy name if last buoy folder
-    if k == length(spotdata)
-        eval([spotname '=dout;']); 
+    %% Now organise to hourly blocks
+    clc; 
+    disp('Hourly blocking'); 
+    eval(['dt = datevec(' spotname{s} '.time);']); 
+    eval(['dbuoy = ' spotname{s} ';']); 
+    hrs = unique(dt(:,1:4),'rows'); 
+    [r,c] = size(hrs); 
+    %this should be 2.5 hz * 3600 s = 9000
+    sample_rate = 2.5; 
+    blocks = 3600; 
+    dum_block = ones(r,sample_rate*blocks).*nan; 
+    
+    dbuoy.time_hourly = dum_block; 
+    dbuoy.x_hourly = dum_block; 
+    dbuoy.y_hourly = dum_block; 
+    dbuoy.z_hourly = dum_block; 
+    
+    for j = 1:size(hrs,1); 
+        idx = find(dt(:,1) == hrs(j,1) & dt(:,2) == hrs(j,2) & dt(:,3) == hrs(j,3) & dt(:,4) == hrs(j,4)); 
+        dbuoy.time_hourly(j,1:length(idx)) = dbuoy.time(idx)'; 
+        dbuoy.x_hourly(j,1:length(idx)) = dbuoy.x(idx)'; 
+        dbuoy.y_hourly(j,1:length(idx)) = dbuoy.y(idx)'; 
+        dbuoy.z_hourly(j,1:length(idx)) = dbuoy.z(idx)';
     end
-    clear ddisp
+    
+    eval([spotname{s} '=dbuoy;']);
+    clear dbuoy
+    
+    %save somewhere
+    outpath = 'I:\Active_Projects\LOWE_IMOS_WaveBuoys\Data\Displacements_for_final_STAC';
+    savename = [spotname{s} '_displacements.mat']; 
+    savefile = fullfile(outpath,savename); 
+    
+    eval(['save(' savefile '),' spotname{s} ', -v7.3']); 
 end
 
-%% Now organise to hourly blocks 
-eval(['dt = datevec(' spotname '.timeutc);']); 
-eval(['dbuoy = ' spotname ';']); 
-hrs = unique(dt(:,1:4),'rows'); 
-[r,c] = size(hrs); 
-dhourly = struct('timeutc',ones(r,10000).*nan,'x',ones(r,10000).*nan,'y',ones(r,10000).*nan,'z',ones(r,10000).*nan); 
-for j = 1:size(hrs,1); 
-    idx = find(dt(:,1) == hrs(j,1) & dt(:,2) == hrs(j,2) & dt(:,3) == hrs(j,3) & dt(:,4) == hrs(j,4)); 
-    dhourly.timeutc(j,1:length(idx)) = dbuoy.timeutc(idx)'; 
-    dhourly.x(j,1:length(idx)) = dbuoy.x(idx)'; 
-    dhourly.y(j,1:length(idx)) = dbuoy.y(idx)'; 
-    dhourly.z(j,1:length(idx)) = dbuoy.z(idx)';
-end
+
+
+
 
     
     
