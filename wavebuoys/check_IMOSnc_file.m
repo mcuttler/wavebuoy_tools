93bc6a22bf51b17044e703ff83475a3f86fb28d2
Matt Hatcher <matt.hatcher@uwa.edu.au>
2022-09-07 13:36:19 +0800

    Update check_IMOSnc_file.m
    Add more plots for spreading, and fix few problems in code of check_IMOSnc_file.m

--- wavebuoys/check_IMOSnc_file.m
+++ wavebuoys/check_IMOSnc_file.m
@@ -64,15 +64,21 @@ fid = figure;
 ax(1) = subplot(3,1,1); 
 h(1) = plot(data.time, data.hs);
 hold on; grid on;
+if any(data.quality_flag==3)
 h(2) = plot(data.time(data.quality_flag==3), data.hs(data.quality_flag==3),'co'); 
+end
+if any(data.quality_flag==4)
 h(3) = plot(data.time(data.quality_flag==4), data.hs(data.quality_flag==4),'ro'); 
+end
 datetick('x','dd-mmm'); 
 text(0.05, 0.95,'(a)','units','normalized'); 
 ylabel('Hs (m)'); 
 xlabel('Date'); 
+
+if any(data.quality_flag==3) & any(data.quality_flag==4)
 l = legend(h,{'Data','Suspect','Fail'}); 
 %     'Flagged Data (' num2str(round(length(find(data.quality_flag>1))./length(data.quality_flag)*100,2)) '%)']}); 
-
+end
 ax(2) = subplot(3,1,2); 
 plot(data.time, data.tp); 
 hold on; grid on; 
@@ -103,13 +109,14 @@ set(fid, 'PaperPositionMode', 'manual','PaperUnits','centimeters','units','centi
 %%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%
 
-return
+
 %additional plots 
 
 finfo = ncinfo(ncfile);
 
+%return
 
-%temp data
+%temp data, don;t include for non temp SPotters
 data.TEMP = ncread(ncfile,'TEMP'); 
 data.TEMP_quality_flag = ncread(ncfile,'TEMP_quality_flag'); 
 data.TEMP_subflag = ncread(ncfile,'TEMP_subflag'); 
@@ -131,6 +138,10 @@ scatter((1:length(data.time)),data.TEMP_subflag)
 title('TEMP subflag')
 xlim([0 length(data.time)])
 
+
+%%%%%%%%%%%         End Temp Data
+
+
 % percentage of 4 and 3 flags
 
 flag_per.bad = length(find(data.quality_flag==4))/length(data.quality_flag);
@@ -141,7 +152,17 @@ disp(num2str(flag_per.bad))
 disp('fraction of data with flag 3')
 disp(num2str(flag_per.suspect))
 
+%Calc percentage of subflags
 
+quality_subflag_ind = zeros(37,1);
+quality_subflag_per = zeros(37,1);
+
+for ii= 0:36
+    quality_subflag_ind(ii+1)=ii;
+    
+    quality_subflag_per(ii+1) = (sum(data.quality_subflag==ii)/(sum(not(isnan(data.quality_subflag)))))*100;
+    
+end
 
 
 %plot positions
@@ -238,4 +259,50 @@ scatter((1:length(data.time)), data.quality_subflag);
 hold on; grid on; 
 ylabel('Subflag'); 
 
+
+% plot percentage of each type of subflag
+
+figure()
+title('percentage of subflag values')
+
+bar(quality_subflag_ind,quality_subflag_per)
+xticks(quality_subflag_ind);
+
+% plot other variables not yest looked at 
+
+%raw data figure
+fid = figure; 
+ax(1) = subplot(4,1,1); 
+h(1) = plot(data.time, data.tm);
+hold on; grid on;
+datetick('x','dd-mmm'); 
+text(0.05, 0.95,'(a)','units','normalized'); 
+ylabel('tm (s)'); 
+xlabel('Date'); 
+
+ax(2) = subplot(4,1,2); 
+plot(data.time, data.dm); 
+hold on; grid on; 
+datetick('x','dd-mmm');
+xlabel('Date'); 
+ylabel('dm (deg from)'); 
+
+
+ax(3) = subplot(4,1,3); 
+plot(data.time, data.pkspr); 
+hold on; grid on; 
+datetick('x','dd-mmm');
+xlabel('Date'); 
+ylabel('pkspr (deg)'); 
+
+ax(4) = subplot(4,1,4); 
+plot(data.time, data.meanspr); 
+hold on; grid on; 
+datetick('x','dd-mmm');
+xlabel('Date'); 
+ylabel('meanspr (deg)'); 
+
+set(fid, 'PaperPositionMode', 'manual','PaperUnits','centimeters','units','centimeters',...
+'position',[1 1 24 18], 'PaperPosition', [0 0 24 18])
+%%
 
