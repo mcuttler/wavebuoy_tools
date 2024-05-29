3da5ee98e77310386ce3f4f01f67214d341dfb13
mcuttler <michael.cuttler@uwa.edu.au>
2024-05-29 13:46:37 +0800

    add log message for successful runs

--- wavebuoys/realtime/run_batch_realtime.m
+++ wavebuoys/realtime/run_batch_realtime.m
@@ -40,7 +40,8 @@ for jj = 1:size(buoy_metadata)
     %run the realtime workflow 
 %     disp(['running ' buoy_info.name]); %comment out when running for real 
     try
-        [log_message] = batch_realtime(buoy_info); 
+        [log_message] = batch_realtime(buoy_info);
+        fprintf(flog, [buoy_info.name ': ' log_message ' \n']); 
     catch   
 %         disp([buoy_info.name ' could not be completed \n']); %comment out when running for real 
         %add message to log if a buoy fails 
