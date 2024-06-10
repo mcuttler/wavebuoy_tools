16f7686a36ab14c243c811cc9f0c7a3dca3f8507
mcuttler <michael.cuttler@uwa.edu.au>
2024-06-10 20:26:52 +0800

    manual updates to sync to developing

--- wavebuoys/realtime/realtime_tools/realtime_backup_mat.m
+++ wavebuoys/realtime/realtime_tools/realtime_backup_mat.m
@@ -67,76 +67,76 @@ else
             idx_part = find(dt_part(:,1)==ddv(i,1) & dt_part(:,2)==ddv(i,2)); 
         end
         
-        fields = fieldnames(data);
-        for j = 1:length(fields)
-            if strcmp(fields{j},'qf_bott_temp') |strcmp(fields{j},'qf_sst') |strcmp(fields{j},'surf_temp') | strcmp(fields{j},'bott_temp')|strcmp(fields{j},'temp_time') | strcmp(fields{j},'curr_mag') | strcmp(fields{j},'curr_dir') | strcmp(fields{j},'curr_mag_std') | strcmp(fields{j},'curr_dir_std') | strcmp(fields{j},'w') | strcmp(fields{j},'w_std')                
-                buoy_data.(fields{j})=data.(fields{j})(idx_temp,:); 
-                
-            elseif strcmp(fields{j},'press_std_time')|strcmp(fields{j},'pressure_std')
-                buoy_data.(fields{j}) = data.(fields{j})(idx_press_std,:); 
-                
-            elseif strcmp(fields{j},'press_time')|strcmp(fields{j},'pressure')
-                buoy_data.(fields{j}) = data.(fields{j})(idx_press,:); 
-                
-            elseif strcmp(fields{j},'spec_time')|strcmp(fields{j},'a1')|strcmp(fields{j},'a2')|strcmp(fields{j},'b1')|...
-                    strcmp(fields{j},'b2')|strcmp(fields{j},'direction')|strcmp(fields{j},'directionalSpread')|...
-                    strcmp(fields{j},'frequency')|strcmp(fields{j},'varianceDensity')|strcmp(fields{j},'df')
-                 if strcmp(buoy_info.type,'datawell')
-                    if strcmp(fields{j},'frequency')
-                        buoy_data.(fields{j}) = data.(fields{j}); 
+        %check that all variables have data for that month        
+        if ~isempty(idx_part)&~isempty(idx_spec)&~isempty(idx_press)&~isempty(idx_press_std)&~isempty(idx_temp)&~isempty(idx)
+            fields = fieldnames(data);
+            for j = 1:length(fields)
+                if strcmp(fields{j},'qf_bott_temp') |strcmp(fields{j},'qf_sst') |strcmp(fields{j},'surf_temp') | strcmp(fields{j},'bott_temp')|strcmp(fields{j},'temp_time') | strcmp(fields{j},'curr_mag') | strcmp(fields{j},'curr_dir') | strcmp(fields{j},'curr_mag_std') | strcmp(fields{j},'curr_dir_std') | strcmp(fields{j},'w') | strcmp(fields{j},'w_std')                
+                    buoy_data.(fields{j})=data.(fields{j})(idx_temp,:); 
+                    
+                elseif strcmp(fields{j},'press_std_time')|strcmp(fields{j},'pressure_std')
+                    buoy_data.(fields{j}) = data.(fields{j})(idx_press_std,:); 
+                    
+                elseif strcmp(fields{j},'press_time')|strcmp(fields{j},'pressure')
+                    buoy_data.(fields{j}) = data.(fields{j})(idx_press,:); 
+                    
+                elseif strcmp(fields{j},'spec_time')|strcmp(fields{j},'a1')|strcmp(fields{j},'a2')|strcmp(fields{j},'b1')|...
+                        strcmp(fields{j},'b2')|strcmp(fields{j},'direction')|strcmp(fields{j},'directionalSpread')|...
+                        strcmp(fields{j},'frequency')|strcmp(fields{j},'varianceDensity')|strcmp(fields{j},'df')
+                    if strcmp(buoy_info.type,'datawell')
+                        if strcmp(fields{j},'frequency')
+                            buoy_data.(fields{j}) = data.(fields{j}); 
+                        else
+                            buoy_data.(fields{j}) = data.(fields{j})(idx,:);
+                        end
+                        
                     else
-                        buoy_data.(fields{j}) = data.(fields{j})(idx,:); 
+                        buoy_data.(fields{j}) = data.(fields{j})(idx_spec,:);
                     end
                     
+                    
+                elseif strcmp(fields{j},'part_time')|(contains(fields{j},'sea')&~contains(fields{j},'wind'))|contains(fields{j},'swell')
+                    buoy_data.(fields{j}) = data.(fields{j})(idx_part,:); 
+                    
                 else
-                    buoy_data.(fields{j}) = data.(fields{j})(idx_spec,:);
+                    if size(data.(fields{j}),1)>1
+                        buoy_data.(fields{j})=data.(fields{j})(idx,:);
+                    else
+                        buoy_data.(fields{j})=data.(fields{j});
+                    end
                 end
-                 
-                
-            elseif strcmp(fields{j},'part_time')|(contains(fields{j},'sea')&~contains(fields{j},'wind'))|contains(fields{j},'swell')
-                buoy_data.(fields{j}) = data.(fields{j})(idx_part,:); 
-                
-            else
-                if size(data.(fields{j}),1)>1
-                    buoy_data.(fields{j})=data.(fields{j})(idx,:);
-                else
-                    buoy_data.(fields{j})=data.(fields{j}); 
+            end
+            
+            clear idx idx_temp
+            backup_path = [buoy_info.backup_path '\' buoy_info.name '\mat_archive\' num2str(ddv(i,1))];
+            if isfield(buoy_info,'backup_path2')
+                backup_path2 = [buoy_info.backup_path2 '\' buoy_info.name '\mat_archive\' num2str(ddv(1,1))]; 
+            end
+            filename = [buoy_info.name '_' num2str(ddv(i,1)) num2str(ddv(i,2),'%02d') '.mat'];
+            if strcmp(buoy_info.type,'sofar')
+                SpotData = buoy_data; 
+                save([backup_path '\' filename],'SpotData','-v7.3');
+                if isfield(buoy_info,'backup_path2')
+                    save([backup_path2 '\' filename],'SpotData','-v7.3');
+                end
+            elseif strcmp(buoy_info.type,'datawell')
+                dw_data = buoy_data; 
+                save([backup_path '\' filename],'dw_data','-v7.3');
+                if isfield(buoy_info,'backup_path2')
+                    save([backup_path2 '\' filename],'dw_data','-v7.3');
+                end
+            elseif strcmp(buoy_info.type,'triaxys')
+                axys_data = buoy_data; 
+                save([backup_path '\' filename],'axys_data','-v7.3');
+                if isfield(buoy_info,'backup_path2')
+                    save([backup_path2 '\' filename],'axys_data','-v7.3');
                 end
             end
         end
-        
-        
-        
-        clear idx idx_temp
-        backup_path = [buoy_info.backup_path '\' buoy_info.name '\mat_archive\' num2str(ddv(i,1))];
-        if isfield(buoy_info,'backup_path2')
-            backup_path2 = [buoy_info.backup_path2 '\' buoy_info.name '\mat_archive\' num2str(ddv(1,1))]; 
-        end
-        filename = [buoy_info.name '_' num2str(ddv(i,1)) num2str(ddv(i,2),'%02d') '.mat']; 
-         if strcmp(buoy_info.type,'sofar')
-             SpotData = buoy_data; 
-             save([backup_path '\' filename],'SpotData','-v7.3');
-             if isfield(buoy_info,'backup_path2')
-                 save([backup_path2 '\' filename],'SpotData','-v7.3');
-             end 
-         elseif strcmp(buoy_info.type,'datawell')
-             dw_data = buoy_data; 
-             save([backup_path '\' filename],'dw_data','-v7.3');
-             if isfield(buoy_info,'backup_path2')
-                 save([backup_path2 '\' filename],'dw_data','-v7.3');
-             end
-         elseif strcmp(buoy_info.type,'triaxys')
-             axys_data = buoy_data; 
-             save([backup_path '\' filename],'axys_data','-v7.3');
-             if isfield(buoy_info,'backup_path2')
-                 save([backup_path2 '\' filename],'axys_data','-v7.3');
-             end
-         end
-        
     end
 end
-
 end
+
         
     
         
