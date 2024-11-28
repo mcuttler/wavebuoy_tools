abb0212af836b4143d4a68fce9005445ca3fa4b0
Mike Cuttler <00084142@uwa.edu.au>
2024-11-28 12:10:49 +0800

    update to include smart mooring

--- wavebuoys/sofar/spotter_tools/process_sofar_SD_card.m
+++ wavebuoys/sofar/spotter_tools/process_sofar_SD_card.m
@@ -9,6 +9,7 @@
 %   v3 removes utc offset (apply this later in workflow or will get
 %   confusing) and includes concatenting of other types of files. Also
 %   converts outputs to timetables and uses datetime 
+%v3.1 modified to include smart mooring bristlemouth data 
 
 %Example usage: 
 % sofarpath = '\\drive.irds.uwa.edu.au\SEE-PNP-001\HANSEN_Albany_WaveEnergy_Feasibility_ongoing\Data\WaveBuoys\Sofar\SPOT0168_KingGeorgeSound_20201211_to_20210326'; 
@@ -16,7 +17,7 @@
 % [out]s = process_sofar_SD_card(sofarpath, utcoffset); 
 
 %%
-function [displacements, displacements_hdr, surface_temp, baro, gps, smart_mooring] = process_sofar_SD_card(sofarpath)
+function [displacements, displacements_hdr, surface_temp, baro, gps, smart_mooring_bm, smart_mooring_bm_agg] = process_sofar_SD_card(sofarpath)
 
 %% displacements
 disp('concatenating displacements'); 
@@ -118,32 +119,111 @@ if ~isempty(files)
 else
     baro = nan; 
 end
-%% smart_mooring
-disp('concatenating smart mooring'); 
-files = dir([sofarpath '\*_SMD.csv']); 
+%% smart_mooring --- BristleMouth
+disp('concatenating smart mooring Bristlemouth'); 
+files = dir([sofarpath '\*_SENS_IND.csv']); 
+
 if ~isempty(files)
-% NEED TO WRITE THIS FOR EXAMPLE SMART MOORING 
-% for i = 1:size(files)
-%     dum = readtable(fullfile(files(i).folder, files(i).name),'VariableNamingRule','preserve'); 
-% 
-%     %should only have 2 variables
-%     dum = dum(:,1:2); 
-%     dum.Properties.VariableNames = {'GPSEpoch','baro_pressure'}; 
-% 
-%     dt = datetime(dum.GPSEpoch,'convertfrom','posixtime'); 
-%     dumt = timetable(dum.baro_pressure,'RowTimes',dt,'VariableNames',{'baro_pressure'}); 
-% 
-%     if i == 1
-%         baro = dumt; 
-%     else
-%         baro = [baro; dumt]; 
-%     end
-%     clear dum dt dumt; 
-% 
-% end
+    for i = 1:size(files)    
+        dum = readtable(fullfile(files(i).folder, files(i).name),'VariableNamingRule','preserve');
+        %set common variable names for all BristleMOuth sensor types: 
+        % https://sofarocean.notion.site/Spotter-3-Bristlemouth-SD-Card-Data-Guide-50b2a73cd7d74f4987484152878ddad9
+        varnames = {'bm_node_id','node_position','node_app_name','reading_up_time_millis','reading_time_utc_s','sensor_reading_time_s'}; 
+        
+        if ~isempty(dum)
+            %check instrument type
+            if contains(dum.Var3(1),'soft')
+                varnames = [varnames,'temp_degC']; 
+            elseif contains(dum.Var3(1),'RBR')
+                if strcmp(dum.Var3(1),'RBR.T')
+                    varnames = [varnames,'temp_degC']; 
+                elseif strcmp(dum.Var3(1),'RBR.D')
+                    varnames = [varnames,'pressure_dbar']; 
+                elseif strcmp(dum.Var3(1),'RBR.DT')
+                    varnames = [varnames,'temp_degC','pressure_dbar']; 
+                end
+            elseif contains(dum.Var3(1),'aanderaa')
+                varnames = [varnames, 'abs_speed_cm-s','direction_deg','north_cm-s','east_cm-s','heading_deg',...
+                    'tilt_x_deg','tilt_y_deg','single_ping_std','signal_strength_dB','ping_count','abs_tilt_deg',...
+                    'max_tilt','std_tilt','temp_degC']; 
+            end                
+            
+            dum = dum(:,1:length(varnames));
+            dum.Properties.VariableNames = varnames; 
+            
+            
+            dt = datetime(dum.reading_time_utc_s,'convertfrom','posixtime'); 
+            dumt = table2timetable(dum,'RowTimes',dt); 
+            if i == 1
+                smart_mooring_bm = dumt; 
+            else
+                try
+                    smart_mooring_bm = [smart_mooring_bm; dumt]; 
+                catch
+                    smart_mooring_bm = dumt; 
+                end                
+            end
+        end
+        clear dum dt dumt; 
+    end
 else
-    smart_mooring = nan; 
+    smart_mooring_bm = nan; 
 end
+
+% add the extra 'aggregate' files 
+disp('concatenating smart mooring Bristlemouth agg'); 
+files = dir([sofarpath '\*_SENS_AGG.csv']); 
+
+if ~isempty(files)
+    for i = 1:size(files)    
+        dum = readtable(fullfile(files(i).folder, files(i).name),'VariableNamingRule','preserve');
+        %set common variable names for all BristleMOuth sensor types: 
+        % https://sofarocean.notion.site/Spotter-3-Bristlemouth-SD-Card-Data-Guide-50b2a73cd7d74f4987484152878ddad9
+        varnames = {'bm_node_id','node_position','node_app_name','timestamp_utc','reading_count'}; 
+        if ~isempty(dum)
+            %check instrument type
+            if contains(dum.Var3(1),'soft')
+                varnames = [varnames,'temp_mean_degC']; 
+            elseif contains(dum.Var3(1),'RBR')
+                if strcmp(dum.Var3(1),'RBR.T')
+                    varnames = [varnames,'temp_mean_degC']; 
+                elseif strcmp(dum.Var3(1),'RBR.D')
+                    varnames = [varnames,'pressure_mean_dbar','pressure_std_dbar']; 
+                elseif strcmp(dum.Var3(1),'RBR.DT')
+                    varnames = [varnames,'temp_mean_degC','pressure_mean_dbar','pressure_std_dbar']; 
+                end
+            elseif contains(dum.Var3(1),'aanderaa')
+                varnames = [varnames, 'abs_speed_mean_cm-s','abs_speed_std_cm-s',...
+                    'direction_circ_mean','direction_circ_std','temp_mean_degC','abs_tilt_mean_deg','std_tilt_mean_deg'];     
+            end                
+            
+            dum = dum(:,1:length(varnames));
+            dum.Properties.VariableNames = varnames; 
+            
+            
+            dt = datetime(dum.timestamp_utc,'convertfrom','posixtime'); 
+            dumt = table2timetable(dum,'RowTimes',dt); 
+            if i == 1
+                smart_mooring_bm_agg = dumt; 
+            else
+                try
+                    smart_mooring_bm_agg = [smart_mooring_bm_agg; dumt]; 
+                catch
+                    smart_mooring_bm_agg = dumt; 
+                end
+
+            end
+        end
+        clear dum dt dumt; 
+    end
+else
+    smart_mooring_bm_agg = nan; 
+end
+
+%% smart mooring - legacy - TO DO 
+
+%SMD files 
+
 %% GPS
 disp('concatenating gps positions'); 
 files = dir([sofarpath '\*_LOC.csv']); 
