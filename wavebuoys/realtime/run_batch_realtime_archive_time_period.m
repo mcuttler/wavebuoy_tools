cc2728138c32ff9803bb5875bbea6646c9f4ffa0
Mike Cuttler <00084142@uwa.edu.au>
2025-05-28 06:23:19 +0800

    Update run_batch_realtime_archive_time_period.m

--- wavebuoys/realtime/run_batch_realtime_archive_time_period.m
+++ wavebuoys/realtime/run_batch_realtime_archive_time_period.m
@@ -4,14 +4,14 @@
 
 clear; clc;
 %add wavebuoy_tools to path 
-addpath(genpath('C:\Users\00084142\CUTTLER_GitHub\wavebuoy_tools')); 
+addpath(genpath('D:\CUTTLER_GitHub\wavebuoy_tools')); 
 
 %suppress warnings
 warning('off')
 
 %read in metadata for buoys to run
-dpath = 'X:\CUTTLER_wawaves\Data\wawaves'; 
-dname = 'wawaves_buoy_log_metadata.csv'; 
+dpath = 'X:\CUTTLER_wawaves\Data\vicwaves'; 
+dname = 'vicwaves_buoy_log_metadata.csv'; 
 
 buoy_metadata_master = readtable(fullfile(dpath,dname),'VariableNamingRule','preserve'); 
 
@@ -96,10 +96,10 @@ for dd = 1:size(sites,1)
              disp(['archiving mat files']);      
              if ~isfield(data,'systime')
                  data.systime = data.time; 
-                 data.batteryVoltage = ones(size(data.hsig,1)).*nan; 
-                 data.batteryPower = ones(size(data.hsig,1)).*nan; 
-                 data.humidity = ones(size(data.hsig,1)).*nan; 
-                 data.solarVoltage = ones(size(data.hsig,1)).*nan; 
+                 data.batteryVoltage = ones(size(data.hsig,1),1).*nan; 
+                 data.batteryPower = ones(size(data.hsig,1),1).*nan; 
+                 data.humidity = ones(size(data.hsig,1),1).*nan; 
+                 data.solarVoltage = ones(size(data.hsig,1),1).*nan; 
              end
              realtime_archive_mat(buoy_info, data);          
              
