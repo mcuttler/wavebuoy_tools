f8c532dc1a752a60bb782bf0d4dfa8a78805f87c
Mike Cuttler <00084142@uwa.edu.au>
2025-06-19 11:24:41 +0800

    update new gmail settings

--- wavebuoys/realtime/realtime_tools/spotter_buoy_volt_humid_alert.m
+++ wavebuoys/realtime/realtime_tools/spotter_buoy_volt_humid_alert.m
@@ -22,8 +22,8 @@ if V < V_min % If voltage below voltage min warning
     setpref('Internet','E_mail','wa.wavebuoy.alert@gmail.com');
     setpref('Internet','SMTP_Username','wa.wavebuoy.alert');
     %ADD PASSCODE: rvwqxkuaiaqfarht
-    %Or password: UWAwavebuoys1
-    setpref('Internet','SMTP_Password','rvwqxkuaiaqfarht');
+    %Or password: UWAwavebuoys2
+    setpref('Internet','SMTP_Password','insirgbnvojqsznr');
     props = java.lang.System.getProperties;
     props.setProperty('mail.smtp.auth','true');
     props.setProperty('mail.smtp.socketFactory.class', 'javax.net.ssl.SSLSocketFactory');
@@ -50,7 +50,7 @@ if Humid > Humid_max %If humidity is above set threshhold for warning email
     setpref('Internet','SMTP_Server','smtp.gmail.com');
     setpref('Internet','E_mail','wa.wavebuoy.alert@gmail.com');
     setpref('Internet','SMTP_Username','wa.wavebuoy.alert');
-    setpref('Internet','SMTP_Password','rvwqxkuaiaqfarht');
+    setpref('Internet','SMTP_Password','insirgbnvojqsznr');
     props = java.lang.System.getProperties;
     props.setProperty('mail.smtp.auth','true');
     props.setProperty('mail.smtp.socketFactory.class', 'javax.net.ssl.SSLSocketFactory');
